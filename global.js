433412c6df647991923ab476e670791e8fa40443
Zishan Akhtar <zishankahtar2000@gmail.com>
2023-08-19 17:31:25 +0530

    uploading blog site

---
+++ global.js
@@ -0,0 +1,9 @@
+const blob = document.querySelector('.blob');
+document.body.onpointermove = event =>{
+    const {clientX, clientY} = event
+
+    blob.animate({
+        left :`${clientX}px`,
+        top :`${clientY}px`
+    }, { duration:2000,fill:"forwards" })
+}
